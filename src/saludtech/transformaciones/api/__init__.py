a6b0ec7248fc5d0dacb73bb536c5e4fe62924bb2
Fabian Payan <f.payan@uniandes.edu.co>
2025-02-22 21:28:23 -0500

    Actualizacion ajuste base de datos

--- src/saludtech/transformaciones/api/__init__.py
+++ src/saludtech/transformaciones/api/__init__.py
@@ -7,16 +7,10 @@ from flask_swagger import swagger
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 def registrar_handlers():
-    import aeroalpes.modulos.cliente.aplicacion
-    import aeroalpes.modulos.vuelos.aplicacion
+    import saludtech.transformaciones.modulos.anonimizacion.aplicacion
 
 def importar_modelos_alchemy():
-    import aeroalpes.modulos.cliente.infraestructura.dto
-    import aeroalpes.modulos.hoteles.infraestructura.dto
-    import aeroalpes.modulos.pagos.infraestructura.dto
-    import aeroalpes.modulos.precios_dinamicos.infraestructura.dto
-    import aeroalpes.modulos.vehiculos.infraestructura.dto
-    import aeroalpes.modulos.vuelos.infraestructura.dto
+    import saludtech.transformaciones.modulos.anonimizacion.infraestructura.dto
 
 def comenzar_consumidor():
     """
@@ -26,35 +20,19 @@ def comenzar_consumidor():
     """
 
     import threading
-    import aeroalpes.modulos.cliente.infraestructura.consumidores as cliente
-    import aeroalpes.modulos.hoteles.infraestructura.consumidores as hoteles
-    import aeroalpes.modulos.pagos.infraestructura.consumidores as pagos
-    import aeroalpes.modulos.precios_dinamicos.infraestructura.consumidores as precios_dinamicos
-    import aeroalpes.modulos.vehiculos.infraestructura.consumidores as vehiculos
-    import aeroalpes.modulos.vuelos.infraestructura.consumidores as vuelos
+    import saludtech.transformaciones.modulos.anonimizacion.infraestructura.consumidores as anonimizacion
 
     # Suscripción a eventos
-    threading.Thread(target=cliente.suscribirse_a_eventos).start()
-    threading.Thread(target=hoteles.suscribirse_a_eventos).start()
-    threading.Thread(target=pagos.suscribirse_a_eventos).start()
-    threading.Thread(target=precios_dinamicos.suscribirse_a_eventos).start()
-    threading.Thread(target=vehiculos.suscribirse_a_eventos).start()
-    threading.Thread(target=vuelos.suscribirse_a_eventos).start()
+    threading.Thread(target=anonimizacion.suscribirse_a_eventos).start()
 
     # Suscripción a comandos
-    threading.Thread(target=cliente.suscribirse_a_comandos).start()
-    threading.Thread(target=hoteles.suscribirse_a_comandos).start()
-    threading.Thread(target=pagos.suscribirse_a_comandos).start()
-    threading.Thread(target=precios_dinamicos.suscribirse_a_comandos).start()
-    threading.Thread(target=vehiculos.suscribirse_a_comandos).start()
-    threading.Thread(target=vuelos.suscribirse_a_comandos).start()
+    threading.Thread(target=anonimizacion.suscribirse_a_comandos).start()
 
 def create_app(configuracion={}):
     # Init la aplicacion de Flask
     app = Flask(__name__, instance_relative_config=True)
     
-    app.config['SQLALCHEMY_DATABASE_URI'] =\
-            'sqlite:///' + os.path.join(basedir, 'database.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/transformacionesdb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.secret_key = '9d58f98f-3ae8-4149-a09f-3a8c2012e32c'
@@ -62,10 +40,10 @@ def create_app(configuracion={}):
     app.config['TESTING'] = configuracion.get('TESTING')
 
      # Inicializa la DB
-    from aeroalpes.config.db import init_db
+    from saludtech.transformaciones.config.db import init_db
     init_db(app)
 
-    from aeroalpes.config.db import db
+    from saludtech.transformaciones.config.db import db
 
     importar_modelos_alchemy()
     registrar_handlers()
@@ -76,19 +54,9 @@ def create_app(configuracion={}):
             comenzar_consumidor()
 
      # Importa Blueprints
-    from . import cliente
-    from . import hoteles
-    from . import pagos
-    from . import precios_dinamicos
-    from . import vehiculos
-    from . import vuelos
+    from . import anonimizacion
 
     # Registro de Blueprints
-    app.register_blueprint(cliente.bp)
-    app.register_blueprint(hoteles.bp)
-    app.register_blueprint(pagos.bp)
-    app.register_blueprint(precios_dinamicos.bp)
-    app.register_blueprint(vehiculos.bp)
     app.register_blueprint(vuelos.bp)
 
     @app.route("/spec")
--- src/saludtech/transformaciones/config/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/transformacionesdb')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
